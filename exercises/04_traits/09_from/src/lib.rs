f031a598c39e58294fab69c1d172a757125e795c
Stanislav <st4s1k24@gmail.com>
2024-08-02 01:49:39 +0300

    [ETLR-0409]: Traits - From Trait

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -4,6 +4,14 @@ pub struct WrappingU32 {
     value: u32,
 }
 
+impl From<i32> for WrappingU32 {
+    fn from(other: i32) -> WrappingU32 {
+        WrappingU32 {
+            value: other as u32
+        }
+    }
+}
+
 fn example() {
     let wrapping: WrappingU32 = 42.into();
     let wrapping = WrappingU32::from(42);
