1db0edbe350cf4508fce799030216d1e9e8e89fd
Stanislav <st4s1k24@gmail.com>
2024-08-02 00:08:18 +0300

    [ETLR-0311]: Ticket v1 - Destructors

--- exercises/03_ticket_v1/11_destructor/src/lib.rs
+++ exercises/03_ticket_v1/11_destructor/src/lib.rs
@@ -2,7 +2,11 @@
 // We'll pick the concept up again in a later chapter after covering traits and
 // interior mutability.
 fn outro() -> &'static str {
-    "I have a basic understanding of __!"
+    // let x = String::from("dummy");
+    // drop(x); // value moved here
+    // println!("{x}"); // value borrowed here after move, so it will fail
+    "I have a basic understanding of destructors!"
+    // value dropped here (out of scope) if it wasn't moved or dropped before
 }
 
 #[cfg(test)]
