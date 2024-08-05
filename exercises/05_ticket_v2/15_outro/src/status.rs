bb1336c4cdf7cae0edc4109b623afa3f1a72154c
Stanislav <st4s1k24@gmail.com>
2024-08-06 02:00:51 +0300

    [ETLR-0515]: Ticket v2 - Outro

--- exercises/05_ticket_v2/15_outro/src/status.rs
+++ exercises/05_ticket_v2/15_outro/src/status.rs
@@ -1,16 +1,55 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for the `Status` enum.
 //  The parsing should be case-insensitive.
 
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
+#[derive(Clone, Debug, PartialEq)]
 pub enum Status {
     ToDo,
     InProgress,
     Done,
 }
 
+impl Display for Status {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let message = match self {
+            Status::ToDo
+            | Status::InProgress
+            | Status::Done
+            => "Invalid",
+        };
+        write!(f, "{message}")
+    }
+}
+
+impl Error for Status {
+}
+
+impl TryFrom<String> for Status {
+    type Error = ();
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(())
+        }
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = ();
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Status::try_from(value.to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
     use std::convert::TryFrom;
+
+    use super::*;
 
     #[test]
     fn test_try_from_string() {
