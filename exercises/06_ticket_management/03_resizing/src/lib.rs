61d69f0e62d8d028a4461a424094f78c6f4fae30
Stanislav <st4s1k24@gmail.com>
2024-08-06 02:53:04 +0300

    [ETLR-0603]: Ticket Management - Resizing

--- exercises/06_ticket_management/03_resizing/src/lib.rs
+++ exercises/06_ticket_management/03_resizing/src/lib.rs
@@ -7,11 +7,13 @@ mod tests {
         v.push(2); // max capacity reached
         assert_eq!(v.capacity(), 2);
 
+        let prev_capacity = v.capacity();
         v.push(3); // beyond capacity, needs to resize
+        let new_capacity = prev_capacity * 2; // guessed it from Java
 
         // Can you guess what the new capacity will be?
         // Beware that the standard library makes no guarantees about the
         // algorithm used to resize the vector, so this may change in the future.
-        assert_eq!(v.capacity(), todo!());
+        assert_eq!(v.capacity(), new_capacity);
     }
 }
