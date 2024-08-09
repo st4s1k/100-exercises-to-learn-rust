273754efef0fa51ea2e05edde33286d258da85d0
Stanislav <st4s1k24@gmail.com>
2024-08-10 00:18:22 +0300

    [ETLR-0707]: Threads - Ack pattern - Two-way communication

--- exercises/07_threads/07_ack/src/lib.rs
+++ exercises/07_threads/07_ack/src/lib.rs
@@ -1,13 +1,21 @@
 use std::sync::mpsc::{Receiver, Sender};
-use crate::store::TicketStore;
+
+use crate::data::{Ticket, TicketDraft};
+use crate::store::{TicketId, TicketStore};
 
 pub mod data;
 pub mod store;
 
 // Refer to the tests to understand the expected schema.
 pub enum Command {
-    Insert { todo!() },
-    Get { todo!() }
+    Insert {
+        draft: TicketDraft,
+        response_sender: Sender<TicketId>,
+    },
+    Get {
+        id: TicketId,
+        response_sender: Sender<Option<Ticket>>,
+    },
 }
 
 pub fn launch() -> Sender<Command> {
@@ -21,19 +29,22 @@ pub fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
-            Ok(Command::Insert {}) => {
-                todo!()
+            Ok(Command::Insert {draft, response_sender}) => {
+                let id = store.add_ticket(draft.clone());
+                response_sender.send(id).unwrap();
             }
             Ok(Command::Get {
-                todo!()
-            }) => {
-                todo!()
+                   id,
+                   response_sender
+               }) => {
+                let ticket_option = store.get(id).map(|t| t.clone());
+                response_sender.send(ticket_option).unwrap()
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
                 // and shut down the server.
                 break
-            },
+            }
         }
     }
 }
